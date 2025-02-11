c594cbce9ada0df25ac8a66440c2714f5c4b0aab
Lukesh Bahurupi <lbahurupi567@gmail.com>
2025-02-11 15:55:39 +0530

    commited

--- DayTwo(2)/app.js
+++ DayTwo(2)/app.js
@@ -1,11 +1,18 @@
 var app = angular.module("transflower",[]);
-app.controller("loginController",($scope)=>{
+app.controller("LoginController",($scope)=>{
     //model
     $scope.user={
         email:"lukesh@test.com",
         pasword: "123@test"
     }
-
+    $scope.users=[
+        { email: "lukesh@test.com", password: "123@test" },
+        { email: "johndoe@example.com", password: "password123" },
+        { email: "janedoe@example.org", password: "janedoe@123" },
+        { email: "admin@domain.com", password: "admin1234" },
+        { email: "user@website.com", password: "user@123" }
+      ]
+      
     $scope.Validate = () =>{
         if(true){
             alert('valid user!');
