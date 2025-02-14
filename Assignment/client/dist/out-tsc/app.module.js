68bd03a9ae45e734bb3fc800d4a3eb342e5de2a2
Lukesh Bahurupi <lbahurupi567@gmail.com>
2025-02-14 11:55:07 +0530

    client updated

--- Assignment/client/dist/out-tsc/app.module.js
+++ Assignment/client/dist/out-tsc/app.module.js
@@ -1,2 +1,15 @@
-
-angular.module("myApp", ["authModule", "catlogModule"]);
+angular.module("myApp", ["catlogModule" ])
+    .config(function ($routeProvider) {
+    $routeProvider
+        .when("/catlog", {
+        templateUrl: "src/views/catlog.html",
+        controller: "catlogController"
+    })
+        .when("/auth", {
+        templateUrl: "src/views/auth.html",
+        controller: "authController"
+    })
+        .otherwise({
+        redirectTo: "/catlog"
+    });
+});
--- Assignment/client/src/app.module.ts
+++ Assignment/client/src/app.module.ts
@@ -1,4 +1,17 @@
-import angular from "angular"
+import * as angular from "angular";
 
-
-angular.module("myApp",["authModule","catlogModule"])
+angular.module("myApp",["catlogModule","authModule"])
+.config(($routeProvider: any) => {
+    $routeProvider
+      .when("/catlog", {
+        templateUrl: "src/views/catlog.html",
+        controller: "catlogController"
+      })
+      .when("/auth", {
+        templateUrl: "src/views/auth.html",
+        controller: "authController"
+      })
+      .otherwise({
+        redirectTo: "/catlog"
+      });
+});
